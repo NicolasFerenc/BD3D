49dca204e4845552318df1bb1072135ecdaf4fae
NicoFerenc <f.tico07@gmail.com>
2022-11-28 23:33:01 -0300

    3raentrega

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,101 @@
-const productos=[];
+const productos = [];
+const carrito = [];
 
-function Producto(nombre, precio,stock){
-    this.nombre= nombre;
-    this.precio=parseFloat(precio);
-    this.stock=(stock);    
+const catalogo = document.querySelector("#catalogo");
+const carritoDiv= document.querySelector('.carrito');
+
+class Producto {
+    constructor(id, tipo, precio, url) {
+        this.id = id;
+        this.tipo = tipo;
+        this.precio = parseFloat(precio);
+        this.url = url;
+    }
+    sumarIva() {
+        return this.precio *= 1.21;
+    }
 }
-const productoNuevo1= new Producto("Producto de Estudio", 1500,10);
-const productoNuevo2= new Producto("Producto para el Hogar", 1800,20);
-const productoNuevo3= new Producto("Producto de Hobby", 2000,15);
-const productoNuevo4= new Producto("Productos Coleccionables", 2500,5);
 
-function cargarProductos(producto){
-    productos.push(producto);
+function cargarProductos() {
+    productos.push(new Producto(1, "Producto de Estudio", 1500,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHJk6xsq373zW3yedNKw0zHufyAa9k93QpHw&usqp=CAU"))
+    productos.push(new Producto(2, "Producto para el Hogar", 1800, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeg8IIdQ6eZqhO4gF0vW3bBfgbuVuKu5iNr1VcjSISJQ_Cxl1cgkKwI9meSJm9uhe5fLE&usqp=CAU"));
+    productos.push(new Producto(3, "Producto coleccionable", 6000, "https://bitfab.io/wp-content/uploads/2020/01/funko-venom.jpg"));
+}
+
+cargarProductos();
+
+console.log(productos);
+
+function crearCards() {
+    for (const prod of productos) {
+        prod.sumarIva()
+        let div = document.createElement('div')
+        div.className = "carta"
+        div.innerHTML = `<section id="container" class="containercards">
+                           <h3>${prod.tipo}</h3>
+                           <p>$${prod.precio}</p>
+                           <img class="cartaimg" src="${prod.url}" alt="">
+                           <button class="btnCarrito" id="btn-agregar${prod.id}" >Comprar</button>
+                         </section>
+                         `  
+        catalogo.append(div)
+    }
+    agregarFuncionAlBoton();
+}
+
+function agregarFuncionAlBoton(){
+    productos.forEach(productos => {
+       document.querySelector(`#btn-agregar${productos.id}`).addEventListener("click",()=>{
+        agregarAlCarrito(productos)
+       })
+    });
+}
+
+function agregarAlCarrito(productos){ 
+   
+   let existe =carrito.some(prds=>prds.id === productos.id);
+   if(existe===false){
+    productos.cantidad = 1;
+    carrito.push(productos);
    }
-cargarProductos(productoNuevo1);
-cargarProductos(productoNuevo2);
-cargarProductos(productoNuevo3);
-cargarProductos(productoNuevo4);
-
-
-for (const objeto of productos) {
-    console.log(objeto);
-  }
-
-const inventario= productoNuevo1.stock + productoNuevo2.stock + productoNuevo3.stock+ productoNuevo4.stock; 
-
-console.log("El stock actual de tu inventario es:" + " " + inventario );
-
-  function saludar (usuario){
-    alert("Hola" + " "+ usuario);
-  };
-  let usuario= prompt("Bienvenido a BD3D, ingrese su Usuario");
-  saludar(usuario);
-
-  function pago (cuota){
-    alert("Tu producto sale" + " " + "$" +cuota);
+   else{
+     let productoFind = carrito.find(prds=>prds.id === productos.id)
+     productoFind.cantidad++;
+   }
+   console.log(carrito);
+   renderizarCarrito();
 }
-let producto = prompt("Ingresa una opción \n 1-Producto de Estudio \n 2-Producto para el Hogar \n 3-Producto de Hobby \n 4-Productos Coleccionables ");
-    if(producto==1){
-        precio=1500;
-        console.log(precio);
-    }
-    else if(producto==2){
-        precio=1800;
-        console.log(precio);
-    }
-    else if(producto==3){
-        precio=2000;
-        console.log(precio);
-    }
-    else if(producto==4){
-        precio=2500;
-        console.log(precio);
-    }  
-    else{
-        alert("no elegiste un producto valido");
-    }
-    
-let formadepago = prompt("Ingresa una opción \n 1-efectivo \n 2-transferencia bancaria \n 3-mercado pago ");
-    if(formadepago==1){
-        cuota=precio*.9;
-        console.log(cuota);
-    }
-    else if(formadepago==2){
-        cuota=precio*1.1;
-        console.log(cuota);
-    }
-    else if(formadepago==3){
-        cuota=precio*1.3;
-        console.log(cuota);
-    }
-    else{
-        alert("no elegiste un medio de pago validp");
-    }
-pago(cuota);
+
+function renderizarCarrito(){
+    carritoDiv.innerHTML="";
+     carrito.forEach(prds =>{
+        carritoDiv.innerHTML += `<div id="container" class="containercards">
+                           <h3>${prds.tipo}</h3>
+                           <h3>CANTIDAD: ${prds.cantidad}</h3>
+                           <p>$${prds.precio}</p>
+                           <img class="cartaimg" src="${prds.url}" alt="">
+                           <button class="btnCarrito" id="btn-borrar${prds.id}" >Borrar</button>
+                         </div>
+                         `  
+     })
+     borrarProducto()
+}
+
+function borrarProducto(){
+    carrito.forEach(productos => {
+        document.querySelector(`#btn-borrar${productos.id}`).addEventListener("click",()=>{
+          let indice = carrito.findIndex(e=>e.id===productos.id)
+          carrito.splice(indice,1)
+          renderizarCarrito()
+        })
+     });
+}
+
+function total(){
+    let total = document.getElementById("total")
+    let resultado = carrito.reduce((acc,el)=> acc + el.precio, 0)
+    total.innerText = resultado.toFixed(2)
+}
+total();
+
+crearCards();
